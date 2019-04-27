4a5c9a5b7ac8957d49b3fa93b08dd3bd3324ea93
PramodJana <Pramodjana395@gmail.com>
2019-04-28 02:41:25 +0530

    property binding

--- mastering the basics/binding/src/app/test/test.component.ts
+++ mastering the basics/binding/src/app/test/test.component.ts
@@ -5,27 +5,20 @@ import { Component, OnInit } from '@angular/core';
   template: `<h2>
                   Welcome {{name}}
                 </h2>
-                <h2>{{2+2}}</h2>
-                <h2>{{"Welcome "+name}}</h2>
-                <h2>{{"Length of name is "+name.length}}</h2>
-                <h2>{{name.toUpperCase()}}</h2>
-                <h2>{{greatUser()}}</h2>
-                <h2>{{siteUrl}}</h2>`,
+                <input [id]="myId" type="text" value="Pramod">
+                <input bind-disabled="isDisabled" id="{{myId}}" type="text" value="Pramod">
+                `,
   styles: [ ]
 })
 export class TestComponent implements OnInit {
 
   public name= "Pramod";
-  public siteUrl= window.location.href;
+  public myId = "testId";
+  public isDisabled= true;
   constructor() { }
 
   ngOnInit() {
   }
 
-
-  greatUser()
-  {
-    return "Hello " + this.name;
-  }
 
 }
